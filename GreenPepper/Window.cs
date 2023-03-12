469e5d98d60233b69aed34f9102dcf0a169d5fd4
KYapp <kukiyomenaisapp@gmail.com>
2023-03-12 23:56:49 +0900

    テスト用のWindow

---
+++ GreenPepper/Window.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace GreenPepper;
+
+public class Window
+{
+    public readonly int width;
+    public readonly int height;
+
+    public Window(int width, int height)
+    {
+        this.width = width;
+        this.height = height;
+    }
+
+    public string Build()
+    {
+        StringBuilder builder = new();
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                builder.Append(' ');
+            }
+        }
+
+        return builder.ToString();
+    }
+}

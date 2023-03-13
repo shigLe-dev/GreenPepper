cf2d1fc9095a22c79cd6a1ad27bdebfba8a9ddc5
KYapp <kukiyomenaisapp@gmail.com>
2023-03-13 21:20:06 +0900

    widthが取得できるGPCharの作成

---
+++ GreenPepper/GPChar.cs
@@ -0,0 +1,14 @@
+using Kodnix.Character;
+namespace GreenPepper;
+
+public struct GPChar
+{
+    public readonly string raw;
+    public readonly int width;
+
+    public GPChar(string raw)
+    {
+        this.raw = raw;
+        this.width = EastAsianWidth.GetLength(raw);
+    }
+}

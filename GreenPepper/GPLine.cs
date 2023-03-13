da4399d51ea6862a49f880f344f5516540808205
KYapp <kukiyomenaisapp@gmail.com>
2023-03-13 21:20:35 +0900

    表示範囲で制限できるGPLineの作成

---
+++ GreenPepper/GPLine.cs
@@ -0,0 +1,43 @@
+using System.Text;
+
+namespace GreenPepper;
+
+public struct GPLine
+{
+    private List<GPChar> chars;
+
+    public GPLine()
+    {
+        chars = new();
+    }
+
+    public GPLine(IEnumerable<GPChar> chars)
+    {
+        this.chars = chars.ToList();
+    }
+
+    public string Build(int maxWidth)
+    {
+        StringBuilder builder = new();
+
+        int currentWidth = 0;
+        for (int i = 0; i < chars.Count; i++)
+        {
+            currentWidth += chars[i].width;
+
+            if (currentWidth > maxWidth)
+            {
+                currentWidth -= chars[i].width;
+                break;
+            }
+
+            builder.Append(chars[i].raw);
+        }
+
+        // 余りを足す
+        for (int i = 0; i < maxWidth - currentWidth; i++)
+            builder.Append(' ');
+
+        return builder.ToString();
+    }
+}

27b413dcfa0c4e7c4ce0e240c01d7d063cd32f55
KYapp <kukiyomenaisapp@gmail.com>
2023-03-13 21:45:52 +0900

    SampleProgram

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -1,13 +1,10 @@
 ﻿using System.Text;
-using Kodnix.Character;
 using GreenPepper;
 
 Console.OutputEncoding = Encoding.UTF8;
 
-
-Console.Write(text.Build(Console.WindowWidth, 1));
-
 while (true)
 {
-    Console.WriteLine(EastAsianWidth.GetLength(Console.ReadKey().KeyChar));
+    Console.Write(new Window(Console.WindowWidth, Console.WindowHeight).Build().Build(Console.WindowWidth, Console.WindowHeight));
+    Console.ReadKey(true);
 }

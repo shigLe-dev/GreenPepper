6d3005df939f3f65c3b46f69904d26b4cb94728d
KYapp <kukiyomenaisapp@gmail.com>
2023-05-13 21:03:04 +0900

    サンプル

---
+++ Sample/Program.cs
@@ -0,0 +1,10 @@
+﻿using ShigLe.TUITools;
+using ShigLe.TUITools.Drawers;
+
+var greenPepper = new GreenPepper();
+
+while (true)
+{
+    greenPepper.Draw(new Text("aaa"));
+    Thread.Sleep(1 / 60);
+}

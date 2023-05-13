bcab32ce0fb1e9772c748848e72b622ef887659d
KYapp <kukiyomenaisapp@gmail.com>
2023-05-13 21:02:30 +0900

    GreenPepperを定義

---
+++ GreenPepper/GreenPepper.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace ShigLe.TUITools;
+
+public class GreenPepper
+{
+    public void Draw(IDrawer drawer)
+    {
+        Console.SetCursorPosition(0, 0);
+        Console.CursorVisible = false;
+
+        using var e = drawer.Draw(0, 0, Console.WindowWidth, Console.WindowHeight).GetEnumerator();
+        var stringBuilder = new StringBuilder();
+
+        while (e.MoveNext()) stringBuilder.Append(e.Current);
+
+        Console.Write(stringBuilder.ToString());
+    }
+}

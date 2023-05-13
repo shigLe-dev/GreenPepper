5c094ec50b5b1865f60e896c2bc0eabe7bb09ef2
KYapp <kukiyomenaisapp@gmail.com>
2023-05-13 21:10:43 +0900

    描画時のサイズを指定可能に

--- GreenPepper/GreenPepper.cs
+++ GreenPepper/GreenPepper.cs
@@ -4,16 +4,21 @@ namespace ShigLe.TUITools;
 
 public class GreenPepper
 {
-    public void Draw(IDrawer drawer)
+    public void Draw(int width, int height, IDrawer drawer)
     {
         Console.SetCursorPosition(0, 0);
         Console.CursorVisible = false;
 
-        using var e = drawer.Draw(0, 0, Console.WindowWidth, Console.WindowHeight).GetEnumerator();
+        using var e = drawer.Draw(0, 0, width, height).GetEnumerator();
         var stringBuilder = new StringBuilder();
 
         while (e.MoveNext()) stringBuilder.Append(e.Current);
 
         Console.Write(stringBuilder.ToString());
+    }
+
+    public void Draw(IDrawer drawer)
+    {
+        Draw(Console.WindowWidth, Console.WindowHeight, drawer);
     }
 }

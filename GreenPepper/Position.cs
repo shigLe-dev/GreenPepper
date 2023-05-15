f95fcf90b1c84388cf1553711d70a83ecdb75413
Juha <kukiyomenaisapp@gmail.com>
2023-05-15 15:54:19 +0900

    Positionを定義

---
+++ GreenPepper/Position.cs
@@ -0,0 +1,13 @@
+namespace ShigLe.TUITools;
+
+public struct Position
+{
+    public readonly int x;
+    public readonly int y;
+
+    public Position(int y, int x)
+    {
+        this.y = y;
+        this.x = x;
+    }
+}
